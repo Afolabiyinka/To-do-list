c19c53bcbb1cb489645bcd0abed0111a13dea285
Afolabiyinka <163763149+Afolabiyinka@users.noreply.github.com>
2024-11-28 08:59:26 +0100

    Cleaning up the code

--- script.js
+++ script.js
@@ -1,61 +1,24 @@
-//Changing the theme of the application
+//Getting your elements to work in javascriopt
+const task = document.querySelector("#task");
+const button = document.querySelector("#add-task");
+const completedTasks = document.querySelector("ul");
+const form = document.querySelector("form");
 
-//Getting the elements to change the theme
-const themeContainer = document.querySelector("#theme-change");
-const body = document.body;
-themeContainer.addEventListener("click", function () {
-  if (body.className === "light-mode") {
-    body.className = "dark-mode";
-    themeContainer.innerHTML = `      <img src="/Icon/sun.svg" alt="">
-`;
-  } else {
-    body.className = "light-mode";
-    themeContainer.innerHTML = ` <img src="/Icon/moon.svg" alt=""> `;
-  }
-});
-
-//Getting our elements to work with in javascript
-const ctaBtn = document.getElementById("add-btn");
-const task = document.getElementById("task");
-const taskDate = document.getElementById("task-date");
-
-// Declaring a function to add a task
-function addTask() {
-  const completedTasks = document.getElementById("completed-tasks");
-  let completed = completedTasks.appendChild(document.createElement("li"));
-  completed.innerText = `${task.value} `;
-
-  let completedTime = completedTasks.append(document.createElement("h3"));
-  completedTime.innerText = `${taskDate.value}`;
-
-  let clearTask = completed.appendChild(document.createElement("h1"));
-  clearTask.innerHTML = `   <img src="/Icon/moon.svg" alt=""> `;
+function addTask(e) {
+  let newTask = document.createElement("li");
+  newTask.innerHTML = task.value;
+  completedTasks.appendChild(newTask);
+  
   if (task.value === "") {
-    // task.setAttribute(true)
-    completed.remove();
+    completedTasks.removeChild(newTask);
   }
-  task.value = "";
-  taskDate.value = "";
+  task.value = '';
+  
 }
-
-function storeEverything() {
-  localStorage.setItem("Theme", JSON.stringify(body.className));
-}
-ctaBtn.addEventListener("click", (e) => {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   addTask();
-  storeEverything()
 });
+button.addEventListener("click", addTask);
 
-function getItems (){
-
-  let preferedTheme =JSON.parse(localStorage.getItem("Theme"))
-  body.className = preferedTheme;
-}
-  
-
-window.addEventListener("DOMContentLoaded", () =>{
-  getItems()
-  storeEverything()
-  console.log("Settings ");
-})
+console.log("Hell0");
